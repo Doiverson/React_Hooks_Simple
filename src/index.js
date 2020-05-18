043cf0281f7877587868ad7f5ba52861de2d50c9
ShosukeDoi <doiverson.canada@gmail.com>
2020-05-18 13:59:35 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from '@sentry/browser';
 import App from "./components/App";
+
+Sentry.init({dsn: "https://883a8190caae4c458130ba5b9f34f751@sentry.io/2649151"});
 
 ReactDOM.render(<App/>, document.querySelector("#root"));
